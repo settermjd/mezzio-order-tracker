b3feb54befd1df1ab7445ae999f94f4c7a6594f9
Matthew Setter <matthew@matthewsetter.com>
2025-03-26 15:51:52 +1000

    Implement a 404 Not Found route
    This is a small change that defines a 404 route which can be redirected
    to when a parcel is not found. There might be better ways, but this one
    seems to make most sense to me, and it's quick and easy.

--- config/routes.php
+++ config/routes.php
@@ -38,7 +38,8 @@ use Psr\Container\ContainerInterface;
  */
 
 return static function (Application $app, MiddlewareFactory $factory, ContainerInterface $container): void {
-    $app->get('/', App\Handler\HomePageHandler::class, 'tracker.form');
+    $app->get('/', App\Handler\HomePageHandler::class, 'home');
+    $app->get('/404', \Mezzio\Handler\NotFoundHandler::class, '404.not.found');
     $app->post('/parcel', App\Handler\ParcelTrackerResultsHandler::class, 'tracker.process');
     $app->get('/api/ping', App\Handler\PingHandler::class, 'api.ping');
 };
