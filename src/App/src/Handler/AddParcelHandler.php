b983ef463872135607afd0fc1581c6ad5b9f9f7e
Matthew Setter <matthew@matthewsetter.com>
2025-04-10 17:02:47 +1000

    Add a new handler to add parcels to the underlying data store
    This is the bare essentials that show how to use a service to add a new
    parcel and then fire off an event where listeners respond accordingly.

---
+++ src/App/src/Handler/AddParcelHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use App\Service\ParcelService;
+use Fig\Http\Message\StatusCodeInterface;
+use Laminas\Diactoros\Response\JsonResponse;
+use Laminas\EventManager\EventManagerInterface;
+use Override;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+/**
+ * This class handles adding new parcels to be tracked to the application.
+ *
+ * It doesn't do anything itself, directly. Rather, it triggers an event where the
+ * subscribers to that event do the hard work.
+ */
+readonly class AddParcelHandler implements RequestHandlerInterface
+{
+    public function __construct(
+        private ParcelService $parcelService,
+        private EventManagerInterface $eventManager,
+    ) {
+    }
+
+    #[Override]
+    public function handle(ServerRequestInterface $request): ResponseInterface
+    {
+        $requestData = $request->getParsedBody() ?? [];
+        $parcel      = $this->parcelService->addParcel($requestData);
+
+        $this->eventManager->trigger(eventName: 'newParcel', argv: ['parcel' => $parcel]);
+
+        return new JsonResponse(
+            [
+                'status' => 'queued',
+            ],
+            StatusCodeInterface::STATUS_ACCEPTED
+        );
+    }
+}
