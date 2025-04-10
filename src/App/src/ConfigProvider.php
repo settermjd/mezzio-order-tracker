4c13bcb1419d083cb633aeb90a7a5ee1660dd0f8
Matthew Setter <matthew@matthewsetter.com>
2025-04-10 17:03:56 +1000

    Update the app's DI container
    This change registers services with the DI container for the
    AddParcelHandler, ParcelService, Twilio Rest client, app logger,
    and Doctrine EventManager.

--- src/App/src/ConfigProvider.php
+++ src/App/src/ConfigProvider.php
@@ -4,6 +4,26 @@ declare(strict_types=1);
 
 namespace App;
 
+use App\EventManager\Listeners\AddNewParcel\NewParcelCreatedEmailNotificationListener;
+use App\EventManager\Listeners\AddNewParcel\NewParcelCreatedSMSNotificationListener;
+use App\EventManager\Listeners\AddNewParcel\NewParcelLoggerListener;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Laminas\EventManager\EventManager;
+use Laminas\EventManager\EventManagerInterface;
+use Laminas\ServiceManager\AbstractFactory\ReflectionBasedAbstractFactory;
+use Monolog\Handler\StreamHandler;
+use Monolog\Level;
+use Monolog\Logger;
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
+use SendGrid;
+use SendGrid\Mail\Mail;
+use Twilio\Rest\Client;
+
+use function assert;
+use function is_array;
+
 /**
  * The configuration provider for the App module
  *
@@ -37,6 +57,84 @@ class ConfigProvider
             'factories'  => [
                 Handler\HomePageHandler::class             => Handler\HomePageHandlerFactory::class,
                 Handler\ParcelTrackerResultsHandler::class => Handler\ParcelTrackerResultsHandlerFactory::class,
+                Handler\AddParcelHandler::class            => ReflectionBasedAbstractFactory::class,
+                Service\ParcelService::class               => new class {
+                    public function __invoke(ContainerInterface $container): Service\ParcelService
+                    {
+                        $entityManager = $container->get(EntityManager::class);
+                        assert($entityManager instanceof EntityManagerInterface);
+
+                        return new Service\ParcelService($entityManager);
+                    }
+                },
+                Client::class                              => new class {
+                    public function __invoke(ContainerInterface $container): Client
+                    {
+                        return new Client(
+                            username: $_ENV['TWILIO_ACCOUNT_SID'],
+                            password: $_ENV['TWILIO_AUTH_TOKEN'],
+                        );
+                    }
+                },
+                LoggerInterface::class                     => new class {
+                    public function __invoke(ContainerInterface $container): LoggerInterface
+                    {
+                        return new Logger('app.logger')
+                            ->pushHandler(new StreamHandler(
+                                __DIR__ . "/../../../data/log/app.log",
+                                Level::Debug
+                            ));
+                    }
+                },
+                EventManagerInterface::class               => new class {
+                    public function __invoke(ContainerInterface $container): EventManagerInterface
+                    {
+                        $eventManager = new EventManager();
+
+                        $logger = $container->get(LoggerInterface::class);
+                        assert($logger instanceof LoggerInterface);
+
+                        $entityManager = $container->get(EntityManager::class);
+                        assert($entityManager instanceof EntityManagerInterface);
+
+                        $twilioClient = $container->get(Client::class);
+                        assert($twilioClient instanceof Client);
+
+                        $twilioConfig = $container->get('config')['twilio'];
+                        assert(is_array($twilioConfig));
+
+                        $sendGridConfig = $container->get('config')['sendgrid'];
+                        assert(is_array($sendGridConfig));
+
+                        $eventManager->attach(
+                            eventName: 'newParcel',
+                            listener: new NewParcelLoggerListener($logger),
+                            priority: 100,
+                        );
+
+                        $eventManager->attach(
+                            eventName: 'newParcel',
+                            listener: new NewParcelCreatedSMSNotificationListener(
+                                $twilioClient,
+                                $twilioConfig
+                            ),
+                            priority: 70,
+                        );
+
+                        $eventManager->attach(
+                            eventName: 'newParcel',
+                            listener: new NewParcelCreatedEmailNotificationListener(
+                                new SendGrid($_ENV['SENDGRID_API_KEY']),
+                                new Mail(),
+                                $logger,
+                                $sendGridConfig
+                            ),
+                            priority: 70,
+                        );
+
+                        return $eventManager;
+                    }
+                },
             ],
         ];
     }
