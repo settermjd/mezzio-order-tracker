9773d26f3ca81de7c7861fe046898bf9ffa6367e
Matthew Setter <matthew@matthewsetter.com>
2025-04-11 14:12:45 +1000

    Add a new environment variable for the Twilio WhatsApp number
    This change adds an environment variable to store a Twilio WhatsApp
    number, as well as adding it to the Docker Compose configuration and
    Twilio autoload config file.

--- config/autoload/twilio.global.php
+++ config/autoload/twilio.global.php
@@ -4,8 +4,9 @@ declare(strict_types=1);
 
 return [
     'twilio' => [
-        'account_sid' => $_ENV['TWILIO_ACCOUNT_SID'],
-        'auth_token'  => $_ENV['TWILIO_AUTH_TOKEN'],
-        'sender'      => $_ENV['TWILIO_PHONE_NUMBER'],
+        'account_sid'     => $_ENV['TWILIO_ACCOUNT_SID'],
+        'auth_token'      => $_ENV['TWILIO_AUTH_TOKEN'],
+        'phone_number'    => $_ENV['TWILIO_PHONE_NUMBER'],
+        'whatsapp_number' => $_ENV['TWILIO_WHATSAPP_NUMBER'],
     ],
 ];
